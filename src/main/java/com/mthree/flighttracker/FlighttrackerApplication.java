724395afa2e3b403ebd3124ab8adddafc6eee2e6
Kyle Yannelli <kyleyannelli@gmail.com>
2025-03-24 12:10:53 -0400

    Initial spring boot application.

---
+++ src/main/java/com/mthree/flighttracker/FlighttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mthree.flighttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlighttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlighttrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mthree/flighttracker/FlighttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mthree.flighttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlighttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
