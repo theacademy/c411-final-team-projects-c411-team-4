e1a4c7fd1071e7561df00e9ac0f6ae6b1d1d03b9
Theebika Thiyagaraja Iyer <theebika6@hotmail.com>
2025-03-26 14:44:45 -0400

    Implemented search bar for search flight for map

---
+++ frontend/flight-tracker/src/routes/map/SearchFlight.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { Search, Button } from 'flowbite-svelte';
+    import { SearchOutline } from 'flowbite-svelte-icons';
+  </script>
+  
+  <div class="flex justify-center items-center gap-4 mt-6">
+
+    <span class="font-semibold text-lg text-sky-800 whitespace-nowrap">Search your flight</span>
+  
+    <div class="flex items-center gap-2">
+      <input 
+        type="text"
+        placeholder="Search Flight Number"
+        class="rounded-lg border border-gray-300 p-2.5 w-60 focus:ring-blue-500 focus:border-blue-500"
+      />
+      <Button class="p-2.5 bg-sky-700 text-white rounded-lg">
+        <SearchOutline class="w-5 h-5" />
+      </Button>
+    </div>
+  </div>
