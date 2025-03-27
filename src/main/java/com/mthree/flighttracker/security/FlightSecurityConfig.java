ac72c40f9b7b9ac730bcc8792e6a650418ba5f08
Kyle Yannelli <kyleyannelli@gmail.com>
2025-03-27 10:37:38 -0400

    started jwt filter

---
+++ src/main/java/com/mthree/flighttracker/security/FlightSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.mthree.flighttracker.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class FlightSecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.re)
+    }
+}
